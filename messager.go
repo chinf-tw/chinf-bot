82e229c7b80ec02db7b3d05470d9b6650984ddc2
CHINF <chinf.work@gmail.com>
2018-10-12 12:46:25 +0800

    debug

--- messager.go
+++ messager.go
@@ -6,7 +6,7 @@ import (
 
 //PushMessage 輸出訊息用
 func PushMessage(event *linebot.Event, bot *linebot.Client) {
-	_, err := bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("0x100086 LINE emoji")).Do()
+	_, err := bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("LINE emoji")).Do()
 	if err != nil {
 		print(err)
 	}
