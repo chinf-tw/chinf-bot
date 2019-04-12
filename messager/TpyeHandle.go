ad555d1660453faf72830ddb797fd6a9ea6ff5bf
CHINF <chinf.work@gmail.com>
2019-04-12 20:18:00 +0800

    1.強化錯誤資料的描述準確性 2.用userid代替掉多次使用的event.Source.UserID 3.強化對於加入會員的Postback.Data辨識性 4.新增功能限制要創辦新會員時，只有五分鐘能輸入姓名 5.用SQL FUNCTION來辨識以及創建資料 6.處理MessageHandle會因為得到*linebot.TextMessage而一直被啟用，應該要有再加入會員的操作中才該被啟用 7.讓dbQueryRow統一處理以及回覆資料庫不回傳的Query出問題時的對應狀況。

--- messager/TpyeHandle.go
+++ messager/TpyeHandle.go
@@ -10,66 +10,88 @@ import (
 
 //EventTypeHandle 處理line-bot回應不同事件的對應狀況（例如：line-bot被加好友後的對應處理）
 func EventTypeHandle(event *linebot.Event, db *sql.DB, bot *linebot.Client, _temporaryStorage map[string][]string) {
+	userid := event.Source.UserID
 	switch event.Type {
 
 	case linebot.EventTypeFollow:
 
-		query := fmt.Sprintf("INSERT INTO spotify_user(line_id) VALUES ('%v') RETURNING id;", event.Source.UserID)
-		dbQueryRow(db, query)
-		PushMessage(event.Source.UserID, bot)
+		query := fmt.Sprintf("INSERT INTO spotify_user(line_id) VALUES ('%v') RETURNING id;", userid)
+		dbQueryRow(db, query, userid, bot)
+		PushMessage(userid, bot)
 
 	case linebot.EventTypePostback:
 
-		if event.Postback.Data == fmt.Sprintf("[%v][yes]", event.Source.UserID) {
-			isRepeat := false
-			for _, TUserID := range _temporaryStorage["User_ID"] {
-				if TUserID == event.Source.UserID {
-					isRepeat = true
-					break
-				}
-			}
-			if !isRepeat {
-				_temporaryStorage["User_ID"] = append(_temporaryStorage["User_ID"], event.Source.UserID)
-			}
-			PushMessageSay(event.Source.UserID, bot, "請輸入您的姓名")
+		if event.Postback.Data == fmt.Sprintf("[%v][join member][yes]", userid) {
+			// isRepeat := false
+			// for _, TUserID := range _temporaryStorage["User_ID"] {
+			// 	if TUserID == userid {
+			// 		isRepeat = true
+			// 		break
+			// 	}
+			// }
+			// if !isRepeat {
+			// 	_temporaryStorage["User_ID"] = append(_temporaryStorage["User_ID"], userid)
+			// }
+			query := fmt.Sprintf("select build_JoinMember_cache('%v');", userid)
+			dbQueryRow(db, query, userid, bot)
+			PushMessageSay(userid, bot, "請在“五分鐘內”輸入您的姓名，舉例：\n姓名為“王小明”\n就需輸入：[王小明]")
 		}
 	}
 }
 
 //MessageHandle 處理正在與使用者溝通的事件（例如：取得加入會員的名字）
 func MessageHandle(event *linebot.Event, db *sql.DB, bot *linebot.Client, _temporaryStorage map[string][]string) {
-
-	switch message := event.Message.(type) {
-	case *linebot.TextMessage:
-		var err error
-		for _, TUserID := range _temporaryStorage["User_ID"] {
-			println(TUserID)
-			if TUserID == event.Source.UserID {
-				query := fmt.Sprintf("UPDATE spotify_user SET name = '%v' WHERE line_id = '%v';", message.Text, event.Source.UserID)
-				err = dbQueryRow(db, query)
-			}
-		}
-		var reply string
-		if err != nil {
-			reply = "恭喜成為會員！"
-		} else {
-			reply = "出了一點問題，詢問一下工程師這發生什麼事吧。"
-			log.Println(err)
-		}
-
-		if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply)).Do(); err != nil {
-			log.Print(err)
+	var isPresence *bool
+	query := fmt.Sprintf("SELECT is_presence(%v)", event.Source.UserID)
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&isPresence); err != nil {
+			log.Fatal(err)
 		}
 	}
+	if *isPresence {
+		switch message := event.Message.(type) {
+		case *linebot.TextMessage:
+			// var err error
+			for _, TUserID := range _temporaryStorage["User_ID"] {
+				println(TUserID)
+				if TUserID == event.Source.UserID {
+					query := fmt.Sprintf("UPDATE spotify_user SET name = '%v' WHERE line_id = '%v';", message.Text, event.Source.UserID)
+					dbQueryRow(db, query, event.Source.UserID, bot)
+				}
+			}
+			PushMessageSay(event.Source.UserID, bot, "恭喜成為會員！")
+			// var reply string
+			// if err == nil {
+			// 	reply = "恭喜成為會員！"
+			// } else {
+			// 	reply = "出了一點問題，詢問一下工程師這發生什麼事吧。"
+			// 	log.Println(err)
+			// }
+
+			// if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply)).Do(); err != nil {
+			// 	log.Print(err)
+			// }
+		}
+	}
+
 }
 
-func dbQueryRow(db *sql.DB, query string) (err error) {
-	var userid interface{}
-	if err := db.QueryRow(query).Scan(&userid); err != nil {
+func dbQueryRow(db *sql.DB, query string, userid string, bot *linebot.Client) (err error) {
+	var response interface{}
+	if err := db.QueryRow(query).Scan(&response); err != nil {
+		sayErr := "出了一點問題，詢問一下工程師這發生什麼事吧。"
 		log.Println(query, " ＜＝出問題！\n", err)
+		PushMessageSay(userid, bot, sayErr)
 		return err
 	}
-	println(query)
-	println(userid)
+	// println(query)
+	// println(userid)
+
+	log.Printf("%v 對資料庫進行了 %v，資料庫回應為：%v", userid, query, response)
 	return nil
 }
